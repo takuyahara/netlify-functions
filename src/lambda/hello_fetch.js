b59db1cdd1f93d86589c04de69a3474d4ec4e61c
takuyahara <h.taku86@gmail.com>
2019-03-28 06:46:40 +0900

    go

--- src/lambda/hello_fetch.js
+++ src/lambda/hello_fetch.js
@@ -1,14 +1,14 @@
 import fetch from "node-fetch";
 
 const API_ENDPOINT =
-  "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke";
+  "https://sonarcloud.io/api/measures/component?metricKeys=new_bugs,new_vulnerabilities,new_technical_debt,new_code_smells,new_coverage,coverage,new_duplicated_lines_density,new_duplicated_blocks&component=takuyahara_test-sonar-cloud";
 
 exports.handler = async (event, context) => {
   return fetch(API_ENDPOINT)
     .then(response => response.json())
     .then(data => ({
       statusCode: 200,
-      body: `${data.setup} ${data.punchline} *BA DUM TSSS*`
+      body: `${JSON.stringify(data)} *BA DUM TSSS*`
     }))
     .catch(error => ({ statusCode: 422, body: String(error) }));
 };
