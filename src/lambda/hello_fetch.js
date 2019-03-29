ae7ab963f9f8b3bffd9558064c95d4b60451ac70
takuyahara <h.taku86@gmail.com>
2019-03-29 19:06:54 +0900

    query

--- src/lambda/hello_fetch.js
+++ src/lambda/hello_fetch.js
@@ -1,10 +1,12 @@
 import fetch from "node-fetch";
 
 const API_ENDPOINT =
-  "https://sonarcloud.io/api/measures/component?metricKeys=new_bugs,new_vulnerabilities,new_technical_debt,new_code_smells,new_coverage,coverage,new_duplicated_lines_density,new_duplicated_blocks&component=takuyahara_test-sonar-cloud";
+  "https://sonarcloud.io/api/measures/component?metricKeys=new_bugs,new_vulnerabilities,new_technical_debt,new_code_smells,new_coverage,coverage,new_duplicated_lines_density,new_duplicated_blocks";
 
 exports.handler = async (event, context) => {
-  return fetch(API_ENDPOINT)
+  const component = event.queryStringParameters.component;
+  const api = `${API_ENDPOINT}&component=${component}`;
+  return fetch(api)
     .then(response => response.json())
     .then(data => {
       const newData = {};
